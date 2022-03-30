6648bea722d04f4615deec42c6d0bb1c1dda33fa
Courtney <courtskit@gmail.com>
2022-03-29 20:20:41 -0700

    convert to react

---
+++ blog/src/components/Navbar.js
@@ -0,0 +1,16 @@
+const Navbar = () => {
+  return ( 
+    <nav className="navbar">
+      <h1>Blog</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/parks">Parks</a>
+        <a href="/mindset">Mindset</a>
+        <a href="/favorites">Favorites</a>
+        <a href="/aboutme">About</a>
+      </div>
+    </nav>
+   );
+}
+ 
+export default Navbar;
---
+++ blog/src/components/Parks.js
@@ -0,0 +1,9 @@
+const Parks = () => {
+  return ( 
+    <div>
+      
+    </div>
+   );
+}
+ 
+export default Parks;
